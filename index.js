fba102e1c0534c0d3b5dcfba5ce5930971ec8e6c
Arman Khan <sparmankhan@gmail.com>
2023-04-01 12:26:49 +0600

    video stream route

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const multer = require('multer');
 const PORT = process.env.PORT || 4000;
 const cors = require('cors')
@@ -14,6 +16,37 @@ app.post('/upload', upload.single('video'), (req, res) => {
   res.send(`Video ${originalname} uploaded successfully! Size: ${size} bytes`);
 });
 
+// Route to stream a video
+app.get('/stream/:videoId', (req, res) => {
+  const videoId = req.params.videoId;
+  const videoPath = path.join(__dirname, 'uploads', videoId);
+  const stat = fs.statSync(videoPath);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+
+  if (range) {
+    const parts = range.replace(/bytes=/, '').split('-');
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const chunksize = end - start + 1;
+    const file = fs.createReadStream(videoPath, { start, end });
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    };
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(videoPath).pipe(res);
+  }
+});
 
 // Start the server
 app.listen(PORT, () => {
